3a6a2b592a9ca607a028f25c7a5d6a9398f04788
LeeJuEun <wndms0116@naver.com>
2016-11-13 06:16:51 +0900

    message

---
+++ www/js/index.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    var index;
+
+    $.ajax({
+        url: '/api/question',
+        method: 'GET',
+        headers: {
+            Accept: 'application/json'
+        },
+        success: function (res) {
+            console.log(res);
+            index = res.result.index;
+            $('#question').html(res.result.question);
+        }
+    });
+
+    var submitAnswer = function () {
+        var inputVal = $('#answer').val();
+        console.log(inputVal);
+        $.ajax({
+            url: '/api/answer',
+
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            data: JSON.stringify({
+                index: index,
+                answer: inputVal
+            }),
+            success: function (res) {
+                console.log(index);
+                console.log(res.result);
+                if (res.result.isCorrect) {
+                    $('.correct').css('display','block');
+                    $('.wrong').css('display','none');
+                } else {
+                    $('.correct').css('display','none');
+                    $('.wrong').css('display','block');
+                }
+
+            }
+        });
+    };
+
+    $('#answer').on('keyup', function (e) {
+        console.log(e.keyCode);
+        if (e.keyCode === 13) submitAnswer();
+    });
+    $('#submit').on('click', submitAnswer);
+});
